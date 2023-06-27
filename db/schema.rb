a8046573d028db0a1cdf522e8ea1196efbb16a6e
Keven da Silva Gonçalves <dasilvagkeven@gmail.com>
2023-06-27 00:32:50 -0300

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_27_032849) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_27_033132) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,7 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_27_032849) do
     t.string "name"
     t.string "key"
     t.integer "key_type"
-    t.boolean "allowed"
+    t.boolean "allowed", default: false
     t.bigint "door_lock_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
